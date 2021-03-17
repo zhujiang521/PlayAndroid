c78794bbe67f723c5b046bca2236ea9763c1d597
zhujiang2 <57114034@qq.com>
2021-03-17 10:50:54 +0800

    修复文章详情页面 bug

--- app/src/main/java/com/zj/play/compose/common/article/ArticlePage.kt
+++ app/src/main/java/com/zj/play/compose/common/article/ArticlePage.kt
@@ -3,16 +3,19 @@ package com.zj.play.compose.common.article
 import android.content.Context
 import android.content.Intent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.blankj.utilcode.util.StringUtils
@@ -20,9 +23,10 @@ import com.zj.core.util.getHtmlText
 import com.zj.core.util.showToast
 import com.zj.model.room.entity.Article
 import com.zj.play.R
-import com.zj.play.compose.repository.CollectRepository
 import com.zj.play.compose.common.BookmarkButton
 import com.zj.play.compose.common.PlayAppBar
+import com.zj.play.compose.repository.CollectRepository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -58,12 +62,9 @@ fun ArticleScreen(
     article: Article?,
     onBack: () -> Unit,
 ) {
-
-    var showDialog by rememberSaveable { mutableStateOf(false) }
-    if (showDialog) {
-        FunctionalityNotAvailablePopup { showDialog = false }
-    }
     val x5WebView = rememberX5WebViewWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+    val collectRepository = CollectRepository()
     Scaffold(
         topBar = {
             PlayAppBar(getHtmlText(article?.title ?: "文章详情"), click = {
@@ -91,7 +92,8 @@ fun ArticleScreen(
         bottomBar = {
             BottomBar(
                 article = article,
-                onUnimplementedAction = { showDialog = true },
+                coroutineScope = coroutineScope,
+                collectRepository = collectRepository
             )
         }
     )
@@ -101,18 +103,16 @@ fun ArticleScreen(
  * Bottom bar for Article screen
  *
  * @param article article
- * @param onUnimplementedAction (event) called when the user performs an unimplemented action
  */
 @Composable
 private fun BottomBar(
     article: Article?,
-    onUnimplementedAction: () -> Unit,
+    coroutineScope: CoroutineScope,
+    collectRepository: CollectRepository,
 ) {
 
     var favoriteIcon by remember { mutableStateOf(if (article?.collect == false) Icons.Filled.FavoriteBorder else Icons.Filled.Favorite) }
     var loadState by remember { mutableStateOf(false) }
-    val coroutineScope = rememberCoroutineScope()
-    val collectRepository = CollectRepository()
 
     Surface(elevation = 2.dp) {
         Row(
@@ -161,14 +161,20 @@ private fun BottomBar(
                 }
             )
             val context = LocalContext.current
-            IconButton(onClick = { sharePost(article?.title ?: "", article?.link ?: "", context) }) {
+            IconButton(onClick = {
+                sharePost(
+                    article?.title ?: "",
+                    article?.link ?: "",
+                    context
+                )
+            }) {
                 Icon(
                     imageVector = Icons.Filled.Share,
                     contentDescription = "分享"
                 )
             }
             Spacer(modifier = Modifier.weight(1f))
-            IconButton(onClick = onUnimplementedAction) {
+            IconButton(onClick = { showToast(R.string.feature_not_available) }) {
                 Icon(
                     painter = painterResource(R.drawable.ic_text_settings),
                     contentDescription = "字体设置"
@@ -176,29 +182,6 @@ private fun BottomBar(
             }
         }
     }
-}
-
-/**
- * Display a popup explaining functionality not available.
- *
- * @param onDismiss (event) request the popup be dismissed
- */
-@Composable
-private fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        text = {
-            Text(
-                text = stringResource(id = R.string.feature_not_available),
-                style = MaterialTheme.typography.body2
-            )
-        },
-        confirmButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = stringResource(id = R.string.dismiss))
-            }
-        }
-    )
 }
 
 /**
