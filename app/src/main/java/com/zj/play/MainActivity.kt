12e6d8100ee182dbaac83c3df6d178ecf44985ce
zhujiang2 <zhujiang2@lenovo.com>
2021-05-24 09:12:21 +0800

    加入状态栏反色.

--- app/src/main/java/com/zj/play/MainActivity.kt
+++ app/src/main/java/com/zj/play/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.zj.play
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
@@ -13,6 +17,10 @@ import com.zj.play.logic.utils.BarUtils
 import com.zj.play.ui.main.NavGraph
 import com.zj.play.ui.theme.PlayAndroidTheme
 import com.zj.play.ui.view.ProvideImageLoader
+import android.app.Activity
+
+
+
 
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +29,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BarUtils.transparentStatusBar(this)
+        setAndroidNativeLightStatusBar()
         setContent {
             PlayAndroidTheme {
                 ProvideWindowInsets {
@@ -31,17 +40,20 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PlayAndroidTheme {
-        Greeting("Android")
+    /**
+     * 状态栏反色
+     */
+    private fun setAndroidNativeLightStatusBar() {
+        val decor = window.decorView
+        val isDark = resources.configuration.uiMode == 0x21
+        if (!isDark) {
+            decor.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            decor.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
     }
+
 }
