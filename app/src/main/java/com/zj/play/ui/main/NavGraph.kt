58ca710d418ff16437a53a32d705373afa899161
zhujiang2 <zhujiang2@lenovo.com>
2021-08-31 09:25:59 +0800

    修改页面跳转动画

--- app/src/main/java/com/zj/play/ui/main/NavGraph.kt
+++ app/src/main/java/com/zj/play/ui/main/NavGraph.kt
@@ -22,16 +22,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.IntOffset
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import com.google.accompanist.navigation.animation.composable
+import androidx.navigation.*
+import androidx.navigation.compose.NamedNavArgument
 import androidx.navigation.compose.navArgument
 import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.gson.Gson
-import com.zj.play.R
 import com.zj.play.logic.model.ArticleModel
 import com.zj.play.logic.utils.getHtmlText
 import com.zj.play.ui.main.PlayDestinations.ARTICLE_ROUTE_URL
@@ -59,29 +57,18 @@ fun NavGraph(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(PlayDestinations.HOME_PAGE_ROUTE,
-            enterTransition = { _, _ ->
-                // Let's make for a really long fade in
-                expandIn(animationSpec = tween(2000))
-            },
-            exitTransition = { _, _ ->
-                // Let's make for a really long fade in
-                shrinkOut(animationSpec = tween(2000))
-            }) {
+        setComposable(
+            PlayDestinations.HOME_PAGE_ROUTE,
+        ) {
             val viewModel: HomeViewModel = viewModel()
             val position by viewModel.position.observeAsState()
             MainPage(actions, position) { tab ->
                 viewModel.onPositionChanged(tab)
             }
         }
-        composable(PlayDestinations.LOGIN_ROUTE, enterTransition = { _, _ ->
-            // Let's make for a really long fade in
-            expandIn(animationSpec = tween(2000))
-        },
-            exitTransition = { _, _ ->
-                // Let's make for a really long fade in
-                shrinkOut(animationSpec = tween(2000))
-            }) {
+        setComposable(
+            PlayDestinations.LOGIN_ROUTE,
+        ) {
             val viewModel: LoginViewModel = viewModel()
             val loginState by viewModel.state.observeAsState()
             LoginPage(actions, loginState, {
@@ -90,19 +77,11 @@ fun NavGraph(
                 viewModel.toLoginOrRegister(it)
             }
         }
-        composable(
+        setComposable(
             "${PlayDestinations.ARTICLE_ROUTE}/{$ARTICLE_ROUTE_URL}",
             arguments = listOf(navArgument(ARTICLE_ROUTE_URL) {
                 type = NavType.StringType
             }),
-            enterTransition = { _, _ ->
-                // Let's make for a really long fade in
-                expandIn(animationSpec = tween(2000))
-            },
-            exitTransition = { _, _ ->
-                // Let's make for a really long fade in
-                shrinkOut(animationSpec = tween(2000))
-            }
         ) { backStackEntry ->
             val arguments = requireNotNull(backStackEntry.arguments)
             val parcelable = arguments.getString(ARTICLE_ROUTE_URL)
@@ -115,6 +94,28 @@ fun NavGraph(
     }
 }
 
+@ExperimentalAnimationApi
+fun NavGraphBuilder.setComposable(
+    route: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    deepLinks: List<NavDeepLink> = emptyList(),
+    content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit
+) {
+    composable(
+        route = route,
+        arguments = arguments,
+        deepLinks = deepLinks,
+        enterTransition = { _, _ ->
+            // Let's make for a really long fade in
+            slideInVertically(animationSpec = tween(800))
+        },
+        exitTransition = { _, _ ->
+            // Let's make for a really long fade in
+            slideOutVertically(animationSpec = tween(800))
+        }, content = content
+    )
+}
+
 /**
  * 对应用程序中的导航操作进行建模。
  */
@@ -125,32 +126,15 @@ class PlayActions(navController: NavHostController) {
         article.title = getHtmlText(article.title)
         val gson = Gson().toJson(article).trim()
         val result = URLEncoder.encode(gson, "utf-8")
-        toAnimView(navController = navController, "${PlayDestinations.ARTICLE_ROUTE}/$result")
+        navController.navigate("${PlayDestinations.ARTICLE_ROUTE}/$result")
     }
 
     val toLogin: () -> Unit = {
-        toAnimView(navController = navController, PlayDestinations.LOGIN_ROUTE)
+        navController.navigate(PlayDestinations.LOGIN_ROUTE)
     }
 
     val upPress: () -> Unit = {
         navController.navigateUp()
-    }
-
-    /**
-     * 跳转动画，之后Navigation如果支持转场动画的话即可生效
-     *
-     * @param navController /
-     * @param route 跳转路径
-     */
-    private fun toAnimView(navController: NavHostController, route: String) {
-        navController.navigate(route) {
-//            anim {
-//                enter = R.anim.activity_push_in
-//                exit = R.anim.activity_push_out
-//                popEnter = R.anim.center_zoom_in
-//                popExit = R.anim.center_zoom_out
-//            }
-        }
     }
 
 }
