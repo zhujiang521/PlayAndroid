e5cb2c79ebebc68abd346c35e31db356405b9377
zhujiang2 <zhujiang2@lenovo.com>
2021-09-28 14:20:37 +0800

    优化搜索框样式

--- app/src/main/java/com/zj/play/ui/page/search/SearchPage.kt
+++ app/src/main/java/com/zj/play/ui/page/search/SearchPage.kt
@@ -1,13 +1,13 @@
 package com.zj.play.ui.page.search
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.insets.statusBarsHeight
 
 @Composable
@@ -48,21 +49,52 @@ fun SearchBar(back: () -> Unit, searchArticle: (String) -> Unit) {
                     contentDescription = "返回"
                 )
             }
-            BasicTextField(
-                value = value,
-                onValueChange = { value = it },
-                maxLines = 1,
-                textStyle = MaterialTheme.typography.subtitle1,
+            Box(
                 modifier = Modifier
-                    .weight(1f)
+                    .weight(5f)
                     .fillMaxWidth()
-                    .wrapContentWidth(Alignment.CenterHorizontally)
                     .padding(5.dp)
                     .border(
-                        border = BorderStroke(1.dp, Color.Blue),
+                        border = BorderStroke(1.dp, Color.Gray),
                         shape = MaterialTheme.shapes.medium
                     )
-            )
+                    .height(43.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                if (value.isEmpty()) {
+                    Text(
+                        text = "请输入搜索内容",
+                        fontSize = 16.sp,
+                        color = Color.Gray,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                } else {
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            imageVector = Icons.Rounded.Close, contentDescription = "",
+                            modifier = Modifier
+                                .clickable {
+                                    value = ""
+                                }
+                                .fillMaxHeight()
+                                .padding(end = 5.dp)
+                        )
+                    }
+                }
+                BasicTextField(
+                    value = value,
+                    onValueChange = { value = it },
+                    maxLines = 1,
+                    textStyle = MaterialTheme.typography.subtitle1,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 5.dp, end = 45.dp)
+                )
+            }
             IconButton(
                 modifier = Modifier.wrapContentWidth(Alignment.End),
                 onClick = {
