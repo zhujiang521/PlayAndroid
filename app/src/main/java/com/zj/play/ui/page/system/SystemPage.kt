a57d663cd266e8a795b79ea268a45aa22be390cd
zhujiang2 <zhujiang2@lenovo.com>
2021-10-21 09:15:07 +0800

    新增一些天气动画

--- app/src/main/java/com/zj/play/ui/page/system/SystemPage.kt
+++ app/src/main/java/com/zj/play/ui/page/system/SystemPage.kt
@@ -4,20 +4,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieComposition
+import com.airbnb.lottie.compose.*
 import com.zj.play.R
 import com.zj.play.ui.view.PlayAppBar
 
 @Composable
 fun SystemPage() {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.weather_rain))
-    val progress by animateLottieCompositionAsState(composition)
-    Column(modifier = Modifier.fillMaxSize()) {
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+
+    val composition1 by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.weather_stormshowersday))
+    val progress1 by animateLottieCompositionAsState(
+        composition = composition1,
+        iterations = LottieConstants.IterateForever
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         PlayAppBar(title = "体系", showBack = false)
         LottieAnimation(composition = composition, progress = progress)
+        LottieAnimation(composition = composition1, progress = progress1)
     }
 }
