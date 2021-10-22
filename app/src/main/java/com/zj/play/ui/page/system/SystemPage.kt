fa42bf0c3d8ed549d605cf70cdc91f0fa84cc96d
zhujiang2 <zhujiang2@lenovo.com>
2021-10-22 10:32:32 +0800

    优化体系样式

--- app/src/main/java/com/zj/play/ui/page/system/SystemPage.kt
+++ app/src/main/java/com/zj/play/ui/page/system/SystemPage.kt
@@ -4,20 +4,25 @@ import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zj.banner.utils.ImageLoader
+import com.zj.play.R
 import com.zj.play.logic.model.AndroidSystemModel
 import com.zj.play.logic.model.PlayState
-import com.zj.play.ui.page.search.Chip
+import com.zj.play.ui.theme.Shapes
 import com.zj.play.ui.view.PlayAppBar
 import com.zj.play.ui.view.lce.LcePage
 
@@ -56,17 +61,34 @@ fun SystemPage(
                     state = listState,
                     modifier = Modifier.weight(1f)
                 ) {
+
                     items(it) { systemModel ->
-                        Chip(text = systemModel.name, modifier = Modifier
-                            .padding(8.dp)
+                        val modifiers = Modifier
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .padding(bottom = 5.dp)
+                            .shadow(1.dp, shape = Shapes.small)
                             .background(
                                 if (systemState.id == systemModel.id) {
-                                    Color.Red
-                                } else Color.Blue
+                                    Color.Gray
+                                } else MaterialTheme.colors.background
                             )
                             .clickable {
                                 systemState = systemModel
-                            })
+                            }
+
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            modifier = modifiers
+                        ) {
+                            Text(
+                                text = systemModel.name,
+                                fontSize = 15.sp,
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
                         if (systemState.id == 0) {
                             systemState = systemModel
                         }
@@ -77,14 +99,32 @@ fun SystemPage(
                     state = childrenState,
                     modifier = Modifier
                         .fillMaxSize()
-                        .weight(1.5f)
+                        .weight(2f)
                 ) {
                     items(systemState.children) { systemModel ->
-                        Chip(text = systemModel.name, modifier = Modifier
-                            .padding(8.dp)
-                            .clickable {
-                                loadArticle(systemModel.id, systemModel.name)
-                            })
+                        Column(
+                            modifier = Modifier
+                                .padding(4.dp)
+                                .clickable {
+                                    loadArticle(systemModel.id, systemModel.name)
+                                },
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+
+                            Card(
+                                shape = Shapes.small,
+                            ) {
+                                ImageLoader(
+                                    R.drawable.img_default,
+                                    Modifier
+                                        .shadow(1.dp, shape = Shapes.small)
+                                        .size(70.dp)
+                                )
+                            }
+
+
+                            Text(text = systemModel.name, fontSize = 14.sp)
+                        }
                     }
                 }
             }
