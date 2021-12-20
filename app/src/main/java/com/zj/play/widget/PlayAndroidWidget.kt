780a2596c1c71bd65078ed507e6c4e4d284116ac
zhujiang2 <zhujiang2@lenovo.com>
2021-12-20 17:16:19 +0800

    新增小部件

---
+++ app/src/main/java/com/zj/play/widget/PlayAndroidWidget.kt
@@ -0,0 +1,45 @@
+package com.zj.play.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import com.zj.play.R
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class PlayAndroidWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.about_me)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.play_android_widget)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
