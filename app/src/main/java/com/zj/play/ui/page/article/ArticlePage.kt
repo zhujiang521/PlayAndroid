a4894563857b7e80f7673c2e47552da2f24e7ad9
zhujiang2 <zhujiang2@lenovo.com>
2022-02-22 10:50:58 +0800

    将 WebView 修改为 Compose 专用的

--- app/src/main/java/com/zj/play/ui/page/article/ArticlePage.kt
+++ app/src/main/java/com/zj/play/ui/page/article/ArticlePage.kt
@@ -3,10 +3,6 @@ package com.zj.play.ui.page.article
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
@@ -15,13 +11,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 import com.zj.play.R
 import com.zj.play.logic.model.ArticleModel
 import com.zj.play.logic.utils.getHtmlText
 import com.zj.play.ui.view.PlayAppBar
-import com.zj.play.ui.view.rememberWebViewWithLifecycle
-
 
 /**
  * Stateless Article Screen that displays a single post.
@@ -36,16 +31,12 @@ fun ArticlePage(
     onBack: () -> Unit,
 ) {
     val context = LocalContext.current
-    val webView = rememberWebViewWithLifecycle()
+    val url = article?.link ?: "https://www.baidu.com"
+    val state = rememberWebViewState(url = url)
     Scaffold(
         topBar = {
             PlayAppBar(getHtmlText(article?.title ?: "文章详情"), click = {
-                if (webView.canGoBack()) {
-                    //返回上个页面
-                    webView.goBack()
-                } else {
-                    onBack.invoke()
-                }
+                onBack.invoke()
             }, showRight = true, rightImg = Icons.Filled.Share, rightClick = {
                 sharePost(
                     article?.title,
@@ -55,33 +46,13 @@ fun ArticlePage(
             })
         },
         content = {
-            AndroidView(
-                factory = { webView },
-                modifier = Modifier
-                    .fillMaxSize()
-            ) { view ->
-                view.webViewClient = object : WebViewClient() {
-                    override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
-                        return try {
-                            if (url.startsWith("http:") || url.startsWith("https:")) {
-                                view!!.loadUrl(url)
-                            } else {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                                webView.context.startActivity(intent)
-                            }
-                            true
-                        } catch (e: Exception) {
-                            false
-                        }
-                    }
+            WebView(
+                state = state,
+                modifier = Modifier.fillMaxSize(),
+                onCreated = { webView ->
+                    webView.settings.javaScriptEnabled = true
                 }
-                val settings: WebSettings = view.settings
-                settings.mixedContentMode =
-                    WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-                settings.javaScriptEnabled = true //启用js
-                settings.blockNetworkImage = false //解决图片不显示
-                view.loadUrl(article?.link ?: "")
-            }
+            )
         }
     )
 }
