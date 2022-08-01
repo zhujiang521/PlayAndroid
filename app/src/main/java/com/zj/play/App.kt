8103ca0e52f29535df049e990e4fc2879ea2d272
zhujiang2 <zhujiang2@lenovo.com>
2022-08-01 22:15:19 +0800

    WebView 适配深色模式.
    去除TBS，使用原生WebView.

--- app/src/main/java/com/zj/play/App.kt
+++ app/src/main/java/com/zj/play/App.kt
@@ -3,16 +3,16 @@ package com.zj.play
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
-import android.util.Log
 import com.scwang.smart.refresh.footer.ClassicsFooter
 import com.scwang.smart.refresh.header.ClassicsHeader
 import com.scwang.smart.refresh.layout.SmartRefreshLayout
-
 import com.tencent.bugly.crashreport.CrashReport
-import com.tencent.smtt.sdk.QbSdk
 import com.zj.core.Play
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.system.exitProcess
 
@@ -37,7 +37,6 @@ class App : Application() {
 
     private fun initData() {
         CoroutineScope(Dispatchers.IO + SupervisorJob()).launch {
-            initQbSdk()
             initBugLy()
         }
     }
@@ -45,19 +44,6 @@ class App : Application() {
     private fun initBugLy() {
         // Bugly bug上报
         CrashReport.initCrashReport(applicationContext, "0f4f8e06b4", false)
-    }
-
-    private fun initQbSdk() {
-        // x5内核初始化接口
-        QbSdk.initX5Environment(applicationContext, object : QbSdk.PreInitCallback {
-            override fun onViewInitFinished(arg0: Boolean) { //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-                Log.e("APP", " onViewInitFinished is $arg0")
-            }
-
-            override fun onCoreInitFinished() {
-                Log.e("APP", " onCoreInitFinished")
-            }
-        })
     }
 
     /**
--- app/src/main/java/com/zj/play/article/X5WebView.kt
+++ app/src/main/java/com/zj/play/article/X5WebView.kt
@@ -1,9 +1,9 @@
 package com.zj.play.article
 
 import android.annotation.SuppressLint
-import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.text.TextUtils
 import android.util.AttributeSet
@@ -12,19 +12,19 @@ import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.webkit.*
+import android.webkit.WebSettings.FORCE_DARK_OFF
 import android.widget.ProgressBar
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
-import com.tencent.smtt.sdk.*
-import com.zj.core.util.showToast
 import com.zj.play.R
-import java.util.*
 
 /**
  * [setShowProgress]
  */
 class X5WebView @JvmOverloads constructor(
-    context: Context?,
+    context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : WebView(context, attrs, defStyleAttr) {
@@ -36,62 +36,45 @@ class X5WebView @JvmOverloads constructor(
     }
 
     init {
-//        x5WebViewExtension.setScrollBarFadingEnabled(false);
         isHorizontalScrollBarEnabled = false //水平不显示小方块
         isVerticalScrollBarEnabled = false //垂直不显示小方块
 
-//      setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);//滚动条在WebView内侧显示
-//      setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);//滚动条在WebView外侧显示
         progressBar = ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal)
         progressBar?.max = 100
         progressBar?.progressDrawable =
             ResourcesCompat.getDrawable(resources, R.drawable.color_progressbar, null)
         addView(progressBar, LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 6))
-//        imageView = new ImageView (getContext());
-//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//        //      加载图 根据自己的需求去集成使用
-//        imageView.setImageResource(R.mipmap.splash);
-//        imageView.setVisibility(VISIBLE);
-//        addView(
-//            imageView,
-//            new LayoutParams (ViewGroup.LayoutParams.MATCH_PARENT,
-//            ViewGroup.LayoutParams.MATCH_PARENT
-//        ))
         initWebViewSettings()
     }
 
     //   基本的WebViewSetting
     @SuppressLint("ClickableViewAccessibility", "SetJavaScriptEnabled")
     private fun initWebViewSettings() {
-        setBackgroundColor(resources.getColor(android.R.color.white, null))
+        setBackgroundColor(resources.getColor(R.color.article_share_bg, null))
         webViewClient = client
         webChromeClient = chromeClient
         setDownloadListener(downloadListener)
         isClickable = true
         setOnTouchListener { _: View?, _: MotionEvent? -> false }
         val webSetting = settings
-//        webSetting.javaScriptEnabled = true
         webSetting.builtInZoomControls = true
         webSetting.javaScriptCanOpenWindowsAutomatically = true
         webSetting.domStorageEnabled = true
         webSetting.allowFileAccess = true
-        webSetting.layoutAlgorithm = WebSettings.LayoutAlgorithm.NARROW_COLUMNS
         webSetting.setSupportZoom(true)
         webSetting.useWideViewPort = true
         webSetting.setSupportMultipleWindows(true)
         webSetting.setAppCacheEnabled(true)
         webSetting.setGeolocationEnabled(true)
-//        webSetting.setAppCacheMaxSize(Long.MAX_VALUE)
-//        webSetting.pluginState = WebSettings.PluginState.ON_DEMAND
-//        webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH)
-        //android 默认是可以打开_bank的，是因为它默认设置了WebSettings.setSupportMultipleWindows(false)
-        //在false状态下，_bank也会在当前页面打开……
-        //而x5浏览器，默认开启了WebSettings.setSupportMultipleWindows(true)，
-        // 所以打不开……主动设置成false就可以打开了
+
+        if (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) { //判断如果系统是深色主题
+            webSetting.forceDark = WebSettings.FORCE_DARK_ON //强制开启webview深色主题模式
+        } else {
+            webSetting.forceDark = FORCE_DARK_OFF
+        }
+
         //需要支持多窗体还需要重写WebChromeClient.onCreateWindow
         webSetting.setSupportMultipleWindows(false)
-//        webSetting.cacheMode = WebSettings.LOAD_NORMAL;
-//        settingsExtension.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);//extension
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
@@ -111,15 +94,13 @@ class X5WebView @JvmOverloads constructor(
 
         //监听进度
         override fun onProgressChanged(view: WebView, newProgress: Int) {
-            progressBar!!.progress = newProgress
+            progressBar?.progress = newProgress
             if (progressBar != null && newProgress != 100) {
                 //WebView加载没有完成 就显示我们自定义的加载图
-                progressBar!!.visibility = VISIBLE
+                progressBar?.visibility = VISIBLE
             } else if (progressBar != null) {
-
                 //WebView加载完成 就隐藏进度条,显示WebView
                 progressBar?.visibility = GONE
-                //imageView.setVisibility(GONE);
             }
         }
     }
@@ -134,42 +115,19 @@ class X5WebView @JvmOverloads constructor(
             super.onPageFinished(webView, url)
         }
 
-        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            //返回值是true的时候控制去WebView打开，
-            // 为false调用系统浏览器或第三方浏览器
-            return if (url.startsWith("http") || url.startsWith("https") || url.startsWith("ftp")) {
-                false
-            } else {
-                try {
-                    val intent = Intent()
-                    intent.action = Intent.ACTION_VIEW
-                    intent.data = Uri.parse(url)
-                    view.context.startActivity(intent)
-                } catch (e: ActivityNotFoundException) {
-                    showToast(R.string.no_app)
-                }
-                true
-            }
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            return true
         }
 
-        override fun onLoadResource(webView: WebView, s: String) {
-            super.onLoadResource(webView, s)
-            val reUrl = webView.url + ""
-            val urlList: MutableList<String> = ArrayList()
-            urlList.add(reUrl)
-            val newList: MutableList<String> = ArrayList()
-            for (cd in urlList) {
-                if (!newList.contains(cd)) {
-                    newList.add(cd)
-                }
-            }
-        }
     }
 
     private val downloadListener =
         DownloadListener { url: String?, _: String?, _: String?, _: String?, _: Long ->
             val uri = Uri.parse(url)
             val intent = Intent(Intent.ACTION_VIEW, uri)
-            context?.startActivity(intent)
+            context.startActivity(intent)
         }
 }
